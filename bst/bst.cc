31038bd157814a894c583cb086e3fead56b513ce
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-11-12 10:16:12 -0500

    Implement insert

--- bst/bst.cc
+++ bst/bst.cc
@@ -58,6 +58,38 @@ BST::~BST()
 	Cleanup(root);
 }
 
+void BST::Insert(const int key)
+{
+	Node *current = root;
+
+	if (current == nullptr) {
+		root = new Node(key);
+		return;
+	}
+
+	Node *parent = nullptr;
+	bool left = false;
+
+	while (current != nullptr) {
+		// Only unique keys are allowed
+		if (key == current->key)
+			return;
+
+		parent = current;
+		left = key < current->key;
+		current = left ? current->left : current->right;
+	}
+
+	current = new Node(key);
+
+	if (parent != nullptr) {
+		if (left)
+			parent->left = current;
+		else
+			parent->right = current;
+	}
+}
+
 void BST::Print() const
 {
 	if (root == nullptr)
