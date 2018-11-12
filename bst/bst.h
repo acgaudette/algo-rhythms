b987e75f0fbcb4564f392e87800683a7aa8a4aca
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-11-12 10:13:48 -0500

    Add BST header file

---
+++ bst/bst.h
@@ -0,0 +1,34 @@
+#ifndef BST_H
+#define BST_H
+
+struct BST {
+	struct Node {
+		Node *left;
+		Node *right;
+		int key;
+
+		Node(const int key);
+		void PrintInorder() const;
+	};
+
+	Node *root;
+
+	static void Cleanup(Node *node);
+	static Node *Copy(Node *node);
+
+	BST();
+	BST(const BST &other); // Copy constructor
+	BST(BST &&other); // Move constructor
+	~BST();
+
+	void Insert(const int key);
+	void Remove(const int key);
+	Node *Find(const int key) const;
+	void Print() const;
+
+private:
+	BST &operator=(BST &other); // Copy assignment operator
+	BST &operator=(BST &&other); // Move assignment operator
+};
+
+#endif
