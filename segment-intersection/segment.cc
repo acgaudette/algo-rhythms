bb4538fe4675e89c8500135efead79fff609f38e
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-11-13 11:39:36 -0500

    Make comments more verbose

--- segment-intersection/segment.cc
+++ segment-intersection/segment.cc
@@ -6,14 +6,22 @@ bool Segment::Intersects(const Segment other) const
 	// The line segment at origin
 	Vec2 compare = end - start;
 
-	// Check relative orientation of the other segment's endpoints
+	/* Check relative orientation of the other segment's endpoints;
+	 * if they share a sign, then the entire other segment is on one side
+	 * of the plane partitioned by this line.
+	 */
+
 	Vec2 vs = other.start - start;
 	float o1 = vs.Cross(compare);
 	vs = other.end - start;
 	float o2 = vs.Cross(compare);
 
-	// Compare against the other segment
+	// Similarly, compare this against the other segment
 	compare = other.end - other.start;
+
+	/* If this was a segment-line comparison,
+	 * these checks would be unnecessary.
+	 */
 
 	vs = start - other.start;
 	float t1 = vs.Cross(compare);
