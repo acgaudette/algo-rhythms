a6a259e5a30156ffef57f67974a5e76bd65bb72d
Aaron C Gaudette <gaudetteaaron@gmail.com>
2018-11-13 11:23:42 -0500

    Implement "scalar cross product"

--- segment-intersection/alg.h
+++ segment-intersection/alg.h
@@ -5,6 +5,15 @@ struct Vec2 {
 	float x;
 	float y;
 
+	// "Scalar cross product" of two 2D vectors
+	// (returns magnitude of the orthogonal 3D vector).
+	// Equivalent to the determinant of a 2x2 matrix
+	// with both points as column vectors.
+	inline float Cross(const Vec2 other) const
+	{
+		return x * other.y - y * other.x;
+	}
+
 	inline Vec2 operator+(const Vec2 other) const
 	{
 		return Vec2 {
